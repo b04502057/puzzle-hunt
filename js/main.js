3e168bfc3531921cdeacc125dab7b4f930e03a25
b04502057 <48717192+b04502057@users.noreply.github.com>
2021-02-18 14:36:03 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,45 @@
-document.getElementById("lock").hidden=false;
 
 function myFunction() {
-    var n = document.getElementById('fname').value;
-    var x=document.getElementById('lock');
-    if (n=="Luca") {
-        x.style.display = "block";
+    var n1 = document.getElementById('fname').value;
+    var x1=document.getElementById('q2');
+    if (n1=="7") {
+        x1.style.display = "block";
       } 
-      else if(n=="luca") {
+      else if(n1=="0") {
+          alert("got you");
+      }
+      else if (n1=="fuck"){
+        alert("cool bro")
+      }
+      else if(n1=="1"){
+        alert("use your brain")
+      }
+      else  {
+        alert("nice try");
+      }
+  }
+
+  function myFunction2() {
+    var n2 = document.getElementById('fname2').value;
+    var x2=document.getElementById('q3');
+    if (n2=="tan(x)") {
+        x2.style.display = "block";
+      } 
+      else if(n2=="0") {
+          alert("poor you");
+      }
+      else  {
+        alert("nice try");
+      }
+  }
+
+  function myFunction3() {
+    var n3 = document.getElementById('fname3').value;
+    var x3=document.getElementById('lock');
+    if (n3=="Luca") {
+        x3.style.display = "block";
+      } 
+      else if(n3=="luca") {
           alert("uppercase");
       }
       else  {
