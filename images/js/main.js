0e0362c58e6a710792155667ab58176788544bd9
b04502057 <48717192+b04502057@users.noreply.github.com>
2021-04-30 00:57:12 +0800

    Add files via upload

---
+++ images/js/main.js
@@ -0,0 +1,69 @@
+
+
+function myFunction0() {
+  var n1 = document.getElementById('fname1').value;
+  var x1=document.getElementById('cell');
+  if (n1=="curl") {
+      x1.style.display = "block";
+    } 
+    else if(n1=="divergence") {
+        alert("got you");
+    }
+    else if (n1=="fuck"){
+      alert("cool bro")
+    }
+    else if(n1=="gradient"){
+      alert("use your brain")
+    }
+    else  {
+      alert("nice try");
+    }
+}
+
+function myFunction() {
+    var n1 = document.getElementById('fname').value;
+    var x1=document.getElementById('q2');
+    if (n1=="7") {
+        x1.style.display = "block";
+      } 
+      else if(n1=="0") {
+          alert("got you");
+      }
+      else if (n1=="fuck"){
+        alert("cool bro")
+      }
+      else if(n1=="1"){
+        alert("use your brain")
+      }
+      else  {
+        alert("nice try");
+      }
+  }
+
+  function myFunction2() {
+    var n2 = document.getElementById('fname2').value;
+    var x2=document.getElementById('q3');
+    if (n2=="tan(x)") {
+        x2.style.display = "block";
+      } 
+      else if(n2=="0") {
+          alert("poor you");
+      }
+      else  {
+        alert("nice try");
+      }
+  }
+
+  function myFunction3() {
+    var n3 = document.getElementById('fname3').value;
+    var x3=document.getElementById('lock');
+    if (n3=="Luca") {
+        x3.style.display = "block";
+      } 
+      else if(n3=="luca") {
+          alert("uppercase");
+      }
+      else  {
+        alert("nice try");
+      }
+  }
